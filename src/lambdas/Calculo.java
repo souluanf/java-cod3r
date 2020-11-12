22ca1973b725ccd26df9e06b33ae2843f3bc50eb
Luan Fernandes <hello@luanfernandes.dev>
2020-11-12 18:56:19 -0300

    Interfaces Funcionais #02

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -3,4 +3,12 @@ package lambdas;
 @FunctionalInterface
 public interface Calculo {
     double executar(double a, double b);
+
+    default String legal(){
+        return "legal";
+    }
+
+    static String muitoLegal(){
+        return "muito legal";
+    }
 }
