e719a1d132d2b656c6573781fc957d77c48140f5
Luan Fernandes <hello@luanfernandes.dev>
2020-11-12 21:08:05 -0300

    Foreach Funcional #02

--- src/lambdas/Foreach.java
+++ src/lambdas/Foreach.java
@@ -13,10 +13,22 @@ public class Foreach {
         }
 
         System.out.println("\nLambda #01...");
-        aprovados.forEach(nome -> System.out.println(nome+"!!!"));
+        aprovados.forEach(nome -> System.out.println(nome + "!!!"));
 
-        System.out.println("\nMethod Reference...");
+        System.out.println("\nMethod Reference #1...");
         aprovados.forEach(System.out::println);
 
+
+        System.out.println("\nLambda #02...");
+        aprovados.forEach(nome -> meuImprimir(nome));
+
+        System.out.println("\nMethod Reference #2...");
+        aprovados.forEach(Foreach::meuImprimir);
+
+
+    }
+
+    static void meuImprimir(String nome) {
+        System.out.println("Oi, meu nome é " + nome);
     }
 }
